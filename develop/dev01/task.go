49413e685af2f39c47657eeb4ef8838366d5735c
Carina <iamcarinn1@gmail.com>
2024-11-08 20:33:55 +0300

    Update task.go

--- develop/dev01/task.go
+++ develop/dev01/task.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/beevik/ntp"
+)
+
 /*
 === Базовая задача ===
 
@@ -13,5 +20,11 @@ package main
 */
 
 func main() {
+    time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Failed to get current time: %v\n", err)
+        os.Exit(1)
+    }
+    fmt.Print(time)
 
 }
