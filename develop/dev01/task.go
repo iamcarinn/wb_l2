9f1d37c453711ed92b63a3abc9653a407299cf5f
Carina <iamcarinn1@gmail.com>
2024-11-18 16:51:45 +0300

    Update dev02/task.go base version

--- develop/dev01/task.go
+++ develop/dev01/task.go
@@ -3,28 +3,65 @@ package main
 import (
 	"fmt"
 	"os"
-
-	"github.com/beevik/ntp"
+    "strconv"
 )
 
 /*
-=== Базовая задача ===
+=== Задача на распаковку ===
 
-Создать программу печатающую точное время с использованием NTP библиотеки.Инициализировать как go module.
-Использовать библиотеку https://github.com/beevik/ntp.
-Написать программу печатающую текущее время / точное время с использованием этой библиотеки.
+Создать Go функцию, осуществляющую примитивную распаковку строки, содержащую повторяющиеся символы / руны, например:
+	- "a4bc2d5e" => "aaaabccddddde"
+	- "abcd" => "abcd"
+	- "45" => "" (некорректная строка)
+	- "" => ""
+Дополнительное задание: поддержка escape - последовательностей
+	- qwe\4\5 => qwe45 (*)
+	- qwe\45 => qwe44444 (*)
+	- qwe\\5 => qwe\\\\\ (*)
 
-Программа должна быть оформлена с использованием как go module.
-Программа должна корректно обрабатывать ошибки библиотеки: распечатывать их в STDERR и возвращать ненулевой код выхода в OS.
-Программа должна проходить проверки go vet и golint.
+В случае если была передана некорректная строка функция должна возвращать ошибку. Написать unit-тесты.
+
+Функция должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
 func main() {
-    time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+    input_s := "a4bc2d5e"
+
+    output_s, err := Unpack(input_s)
+
     if err != nil {
-        fmt.Fprintf(os.Stderr, "Failed to get current time: %v\n", err)
+        // ошибочка
         os.Exit(1)
     }
-    fmt.Print(time)
+ 
+    fmt.Printf(output_s + "\n")
+}
 
+func Unpack(input_s string) (string, error) {
+    if len(input_s) == 0 {
+		return "", nil
+	}
+
+    var output_s []rune
+    var rune_add rune
+    var count int
+
+    for _, char := range input_s {
+        if char >= '0' && char <= '9' {
+            count, _ = strconv.Atoi(string(char))
+        } else {
+            rune_add = char
+            output_s = append(output_s, rune_add)
+            count = 1
+        }
+        for i := 0; i < count - 1; i++ {
+			output_s = append(output_s, rune_add)
+		}
+    }
+
+    if len(output_s) == 0 {
+		return "", fmt.Errorf("invalid input string")
+	}
+
+	return string(output_s), nil
 }
